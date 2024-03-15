8eac2b7f221b62952f40ad6c1444d5682883ea89
Tuan Nhat <letuannhat105@gmail.com>
2024-03-15 08:58:38 +0700

    first commit

---
+++ src/main/java/ptithcm/controller/Index.java
@@ -0,0 +1,14 @@
+package ptithcm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Index {
+	@RequestMapping("/")
+	public String index () {
+		return "index";
+	}
+	
+}
