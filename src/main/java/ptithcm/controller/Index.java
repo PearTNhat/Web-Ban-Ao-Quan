2d12627240048c2f280c54c1fb40acc81fed2e14
Nguyen Phuong <n21dccn166@student.ptithcm.edu.vn>
2024-03-16 07:32:02 +0700

    add sql server connection

--- src/main/java/ptithcm/controller/Index.java
+++ src/main/java/ptithcm/controller/Index.java
@@ -1,14 +1,38 @@
 package ptithcm.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Index {
 	@RequestMapping("/")
-	public String index () {
+	public String index(ModelMap model) {
+		String serverName = "PHUONG-HPLAP";
+        String portNumber = "1433";
+        String databaseName = "QLSV";
+        String username = "sa";
+        String password = "kc";
+        
+        String connectionUrl = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";" + "databaseName="
+                + databaseName + ";username=" + username + ";password=" + password + ";";
+
+        try {
+        	Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        	Connection con = DriverManager.getConnection(connectionUrl);
+        	model.addAttribute("connStatus", "Connected to database!");
+        } catch (SQLException | ClassNotFoundException e) {
+			model.addAttribute("connStatus", "Cannot connect to SQL Server =((");
+			e.printStackTrace();
+		}
 		return "index";
 	}
-	
+
 }
